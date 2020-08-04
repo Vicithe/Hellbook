e3f4653735fdffd7aef5fa0c5f21a1ad2e251bc7
Emily Teng <emilyteng55@gmail.com>
2020-08-03 17:38:31 -0700

    Started some scripts

---
+++ New Unity Project/Assets/Scenes/DepressionMeter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DepressionMeter : MonoBehaviour
+{
+    private int depression;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Canvas/Blackscreen").GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        depression = 0;
+    }
+
+    void OnMouseDown()
+    {
+        if (!GameObject.FindWithTag("correctanswer"))
+        {
+            depression++;
+        }
+
+        GameObject.Find("Canvas/BlackScreen").GetComponent<Image>().color = new Color(0, 0, 0, (255 / 10 * depression));
+    }
+}
